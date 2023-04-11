683fa9aa1dc59c685917290036716387f0693b71
Fabrizio112 <fabri.avila3@gmail.com>
2023-04-11 16:42:34 -0300

    Agrega ESLint

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-let $menu = document.querySelector("#menu");
-let contadorDelMenu = 0;
+const $MENU = document.querySelector('#menu')
+let contadorDelMenu = 0
 
-$menu.addEventListener('click', () => {
-    console.log("Apretado")
-    if (contadorDelMenu === 0) {
-        aparecerElMenu();
-        contadorDelMenu++;
-        return;
-    }
-    if (contadorDelMenu === 1) {
-        desaparecerElMenu();
-        contadorDelMenu--;
-        return;
-    }
+$MENU.addEventListener('click', () => {
+  console.log('Apretado')
+  if (contadorDelMenu === 0) {
+    aparecerElMenu()
+    contadorDelMenu++
+    return
+  }
+  if (contadorDelMenu === 1) {
+    desaparecerElMenu()
+    contadorDelMenu--
+  }
 })
 
-
-let $barraDeNavegacion = document.querySelector(".barra-de-navegacion-principal .barra-de-navegacion__derecha")
-function aparecerElMenu() {
-    $barraDeNavegacion.style.display = "flex";
+const $barraDeNavegacion = document.querySelector('.barra-de-navegacion-principal .barra-de-navegacion__derecha')
+function aparecerElMenu () {
+  $barraDeNavegacion.style.display = 'flex'
 }
-function desaparecerElMenu() {
-    $barraDeNavegacion.style.display = "none";
+function desaparecerElMenu () {
+  $barraDeNavegacion.style.display = 'none'
 }
